3b69fe6577d8aa5d9f24c80d301faf32ee4d8cae
JoaquinB <joakin_2087@hotmail.com>
2023-05-19 09:24:03 -0300

    proyecto encaminado

---
+++ script.js
@@ -0,0 +1,52 @@
+let personajes = [];
+personajes[0] = document.getElementById("Marge");
+personajes[1] = document.getElementById("Homero");
+personajes[2] = document.getElementById("Lisa");
+personajes[3] = document.getElementById("Bart");
+
+personajes.forEach((personaje)=>{
+  personaje.addEventListener ("click", () =>{
+
+     
+    const btnM = document.querySelector("#btn");
+    const modalCentro = document.querySelector("#modal");
+    const modalContent = document.querySelector(".modalContent");
+    const contenido = document.querySelector(".contenido");
+    
+    modalCentro.classList.add("active");
+    contenido.innerHTML = "¡Has elegido a " + personaje.id +"!" +" ¡Buena eleccíón!";
+  
+ /*    if ( (personaje) === 1){
+     const Homeroaudio = document.getElementById("HomeroVoz");
+    Homeroaudio.play();
+  } */
+  switch (personajes.indexOf(personaje)) {
+    case 0:
+      const margeAudio = document.getElementById("margeVoz");
+    margeAudio.play();
+      break;
+    case 1:
+    const homeroAudio = document.getElementById("HomeroVoz");
+      homeroAudio.play();
+      break;
+    case 2:
+      const lisaAudio = document.getElementById("lisaVoz");
+      lisaAudio.play();
+      break;
+    case 3:
+      const BartAudio = document.getElementById("bartVoz");
+    BartAudio.play();
+    break;
+    default:
+    console.log(personajes);
+    
+  }
+    
+   
+    btnM.addEventListener("click", (Cerrar) => {
+    modalCentro.classList.remove("active");
+    });
+    })
+    })
+    
+
