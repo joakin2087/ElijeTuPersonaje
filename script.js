cf441aebd3a925c291d638714d49ab5d26ac6195
JoaquinB <joakin_2087@hotmail.com>
2023-06-04 23:24:50 -0300

    correspondecia respuesta correcta entre input y  json

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ personajes[1] = document.getElementById("Homero");
 personajes[2] = document.getElementById("Lisa");
 personajes[3] = document.getElementById("Bart");
 
+
+
+
 personajes.forEach((personaje)=>{
   personaje.addEventListener ("click", () =>{
          
@@ -18,6 +21,9 @@ personajes.forEach((personaje)=>{
    
   let random = Math.floor(Math.random() * 5);
 
+
+  
+
   switch (personajes.indexOf(personaje)) {
     case 0:
       const margeAudio = document.getElementById("margeVoz");
@@ -29,6 +35,11 @@ personajes.forEach((personaje)=>{
     document.getElementById('respuesta1').innerHTML = data.preguntas[0].Marge[random].Opciones[0].texto;
     document.getElementById('respuesta2').innerHTML = data.preguntas[0].Marge[random].Opciones[1].texto;
     document.getElementById('respuesta3').innerHTML = data.preguntas[0].Marge[random].Opciones[2].texto;
+    document.getElementById('Opcion1').value = data.preguntas[0].Marge[random].Opciones[0].result;
+    document.getElementById('Opcion2').value = data.preguntas[0].Marge[random].Opciones[1].result;
+    document.getElementById('Opcion3').value = data.preguntas[0].Marge[random].Opciones[2].result;
+ 
+
   })
 
       break;
@@ -42,6 +53,10 @@ personajes.forEach((personaje)=>{
       document.getElementById('respuesta1').innerHTML = data.preguntas[1].Homero[random].Opciones[0].texto;
       document.getElementById('respuesta2').innerHTML = data.preguntas[1].Homero[random].Opciones[1].texto;
       document.getElementById('respuesta3').innerHTML = data.preguntas[1].Homero[random].Opciones[2].texto;
+      document.getElementById('Opcion1').value = data.preguntas[1].Homero[random].Opciones[0].result;
+      document.getElementById('Opcion2').value = data.preguntas[1].Homero[random].Opciones[1].result;
+      document.getElementById('Opcion3').value = data.preguntas[1].Homero[random].Opciones[2].result;
+    
       })
       break;
     case 2:
@@ -54,6 +69,10 @@ personajes.forEach((personaje)=>{
       document.getElementById('respuesta1').innerHTML = data.preguntas[2].Lisa[random].Opciones[0].texto;
       document.getElementById('respuesta2').innerHTML = data.preguntas[2].Lisa[random].Opciones[1].texto;
       document.getElementById('respuesta3').innerHTML = data.preguntas[2].Lisa[random].Opciones[2].texto;
+      document.getElementById('Opcion1').value = data.preguntas[2].Lisa[random].Opciones[0].result;
+      document.getElementById('Opcion2').value = data.preguntas[2].Lisa[random].Opciones[1].result;
+      document.getElementById('Opcion3').value = data.preguntas[2].Lisa[random].Opciones[2].result;
+      
       })
       break;
     case 3:
@@ -66,25 +85,66 @@ personajes.forEach((personaje)=>{
     document.getElementById('respuesta1').innerHTML = data.preguntas[3].Bart[random].Opciones[0].texto;
     document.getElementById('respuesta2').innerHTML = data.preguntas[3].Bart[random].Opciones[1].texto;
     document.getElementById('respuesta3').innerHTML = data.preguntas[3].Bart[random].Opciones[2].texto;
+    document.getElementById('Opcion1').value = data.preguntas[3].Bart[random].Opciones[0].result;
+    document.getElementById('Opcion2').value = data.preguntas[3].Bart[random].Opciones[1].result;
+    document.getElementById('Opcion3').value = data.preguntas[3].Bart[random].Opciones[2].result;
+  
     })
     break;
     default:
-    console.log(personajes);
+     
+     
+   
+ 
   }
+
+  
+    let form = document.getElementById('formulario');
+    let score = 0
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      let respuesta = document.querySelector('input[name="respuesta"]:checked').value;
+      
+   
+     if(respuesta === "true"){
+      const puntaje = document.getElementById('puntaje');
+      score++;
+      puntaje.style.visibility = "visible";
+      puntaje.innerHTML ="¡Felcitaciones, has acertado! sumas " + score +  " punto(s)";
+      score.parseint;
+      console.log(score);
+      modalCentro.classList.remove("active");
+      if(score === 15){
+        score = 0;
+      }
+    }
+    else{
+      contenido.innerHTML = "¡Sigue intentando, tu puedes!";
+    }
+    })
+
     
+    
+  
+
     btnM.addEventListener("click", (Cerrar) => {
     modalCentro.classList.remove("active");
       })
-    });
-  })
+ })
+})
+          
+     
+ 
+
+
     
       const btnRedes = document.getElementById ("iconos");
       btnRedes.addEventListener('click', (enlace) => {
       const prontosAudio = document.getElementById("vuelvaProntos");
       confirm ('¿Deseas salir del sitio?') ? prontosAudio.play() :  enlace.preventDefault();
       });
-   
-  
+
+    
   
       
        
