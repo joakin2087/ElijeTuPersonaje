249350716ec92f8993c8e3e7f391b6ca254b3ff2
JoaquinB <joakin_2087@hotmail.com>
2023-07-15 14:39:43 -0300

    score beta, disable css y funcion beta

--- script.js
+++ script.js
@@ -10,20 +10,46 @@ const btnM = document.querySelector("#btn");
 const modalCentro = document.querySelector("#modal");
 const modalContent = document.querySelector(".modalContent");
 const contenido = document.querySelector(".contenido");
+let personajecount = 0;
+let disable = false;
+let fallingCount = 0; 
+
 
 personajes.forEach((personaje)=>{
   personaje.addEventListener ("click", () =>{
-    
-    
- 
-    
-    modalCentro.classList.add("active");
-    contenido.innerHTML = "¡Has elegido a " + personaje.id +"!" +" ¡Buena eleccíón!";
 
    
-  let random = Math.floor(Math.random() * 5);
+    const btns = document.getElementById('btns');
+    btns.style.visibility= "visible";
+    personajecount++;
+   
+    
+       
+    while (personajecount ===5){
+     disable = true; 
+   
+         if (disable === true|| score === 5 ){
+          if(fallingCount === 4 ){
+            contenido.innerHTML = "¡ GAME OVER, vuelve a a comenzar el juego!";
+            btns.style.visibility= "hidden";
+            intentos = 2 ; 
+          }
+          personaje.classList.add("disable");
+          personaje.style.pointerEvents = "none";
+          fallingCount++;
+          contenido.innerHTML = "Elije otro personaje" ;
+          disable = false;
+          personajecount = 0;
+      }
+    
+     
+    }
+    
+   
  
 
+    modalCentro.classList.add("active");
+    contenido.innerHTML = "¡Has elegido a " + personaje.id +"!" +" ¡Buena eleccíón!";
 
   
 
@@ -34,16 +60,15 @@ personajes.forEach((personaje)=>{
     fetch('Preguntas.json')
     .then(response => response.json())
     .then(data => {
-    document.getElementById('pregunta').innerHTML = data.preguntas[0].Marge[random].pregunta; 
-    document.getElementById('respuesta1').innerHTML = data.preguntas[0].Marge[random].Opciones[0].texto;
-    document.getElementById('respuesta2').innerHTML = data.preguntas[0].Marge[random].Opciones[1].texto;
-    document.getElementById('respuesta3').innerHTML = data.preguntas[0].Marge[random].Opciones[2].texto;
-    document.getElementById('Opcion1').value = data.preguntas[0].Marge[random].Opciones[0].result;
-    document.getElementById('Opcion2').value = data.preguntas[0].Marge[random].Opciones[1].result;
-    document.getElementById('Opcion3').value = data.preguntas[0].Marge[random].Opciones[2].result;
- 
-
-  })
+    document.getElementById('pregunta').innerHTML = data.preguntas[0].Marge[personajecount].pregunta; 
+    document.getElementById('respuesta1').innerHTML = data.preguntas[0].Marge[personajecount].Opciones[0].texto;
+    document.getElementById('respuesta2').innerHTML = data.preguntas[0].Marge[personajecount].Opciones[1].texto;
+    document.getElementById('respuesta3').innerHTML = data.preguntas[0].Marge[personajecount].Opciones[2].texto;
+    document.getElementById('Opcion1').value = data.preguntas[0].Marge[personajecount].Opciones[0].result;
+    document.getElementById('Opcion2').value = data.preguntas[0].Marge[personajecount].Opciones[1].result;
+    document.getElementById('Opcion3').value = data.preguntas[0].Marge[personajecount].Opciones[2].result;
+   
+  })  
 
       break;
     case 1:
@@ -52,13 +77,13 @@ personajes.forEach((personaje)=>{
       fetch('Preguntas.json')
       .then(response => response.json())
       .then(data => {
-      document.getElementById('pregunta').innerHTML = data.preguntas[1].Homero[random].pregunta; 
-      document.getElementById('respuesta1').innerHTML = data.preguntas[1].Homero[random].Opciones[0].texto;
-      document.getElementById('respuesta2').innerHTML = data.preguntas[1].Homero[random].Opciones[1].texto;
-      document.getElementById('respuesta3').innerHTML = data.preguntas[1].Homero[random].Opciones[2].texto;
-      document.getElementById('Opcion1').value = data.preguntas[1].Homero[random].Opciones[0].result;
-      document.getElementById('Opcion2').value = data.preguntas[1].Homero[random].Opciones[1].result;
-      document.getElementById('Opcion3').value = data.preguntas[1].Homero[random].Opciones[2].result;
+      document.getElementById('pregunta').innerHTML = data.preguntas[1].Homero[personajecount].pregunta; 
+      document.getElementById('respuesta1').innerHTML = data.preguntas[1].Homero[personajecount].Opciones[0].texto;
+      document.getElementById('respuesta2').innerHTML = data.preguntas[1].Homero[personajecount].Opciones[1].texto;
+      document.getElementById('respuesta3').innerHTML = data.preguntas[1].Homero[personajecount].Opciones[2].texto;
+      document.getElementById('Opcion1').value = data.preguntas[1].Homero[personajecount].Opciones[0].result;
+      document.getElementById('Opcion2').value = data.preguntas[1].Homero[personajecount].Opciones[1].result;
+      document.getElementById('Opcion3').value = data.preguntas[1].Homero[personajecount].Opciones[2].result;
     
       })
       break;
@@ -68,13 +93,13 @@ personajes.forEach((personaje)=>{
       fetch('Preguntas.json')
       .then(response => response.json())
       .then(data => {
-      document.getElementById('pregunta').innerHTML = data.preguntas[2].Lisa[random].pregunta; 
-      document.getElementById('respuesta1').innerHTML = data.preguntas[2].Lisa[random].Opciones[0].texto;
-      document.getElementById('respuesta2').innerHTML = data.preguntas[2].Lisa[random].Opciones[1].texto;
-      document.getElementById('respuesta3').innerHTML = data.preguntas[2].Lisa[random].Opciones[2].texto;
-      document.getElementById('Opcion1').value = data.preguntas[2].Lisa[random].Opciones[0].result;
-      document.getElementById('Opcion2').value = data.preguntas[2].Lisa[random].Opciones[1].result;
-      document.getElementById('Opcion3').value = data.preguntas[2].Lisa[random].Opciones[2].result;
+      document.getElementById('pregunta').innerHTML = data.preguntas[2].Lisa[personajecount].pregunta; 
+      document.getElementById('respuesta1').innerHTML = data.preguntas[2].Lisa[personajecount].Opciones[0].texto;
+      document.getElementById('respuesta2').innerHTML = data.preguntas[2].Lisa[personajecount].Opciones[1].texto;
+      document.getElementById('respuesta3').innerHTML = data.preguntas[2].Lisa[personajecount].Opciones[2].texto;
+      document.getElementById('Opcion1').value = data.preguntas[2].Lisa[personajecount].Opciones[0].result;
+      document.getElementById('Opcion2').value = data.preguntas[2].Lisa[personajecount].Opciones[1].result;
+      document.getElementById('Opcion3').value = data.preguntas[2].Lisa[personajecount].Opciones[2].result;
       
       })
       break;
@@ -84,32 +109,34 @@ personajes.forEach((personaje)=>{
     fetch('Preguntas.json')
     .then(response => response.json())
     .then(data => {
-    document.getElementById('pregunta').innerHTML = data.preguntas[3].Bart[random].pregunta; 
-    document.getElementById('respuesta1').innerHTML = data.preguntas[3].Bart[random].Opciones[0].texto;
-    document.getElementById('respuesta2').innerHTML = data.preguntas[3].Bart[random].Opciones[1].texto;
-    document.getElementById('respuesta3').innerHTML = data.preguntas[3].Bart[random].Opciones[2].texto;
-    document.getElementById('Opcion1').value = data.preguntas[3].Bart[random].Opciones[0].result;
-    document.getElementById('Opcion2').value = data.preguntas[3].Bart[random].Opciones[1].result;
-    document.getElementById('Opcion3').value = data.preguntas[3].Bart[random].Opciones[2].result;
+    document.getElementById('pregunta').innerHTML = data.preguntas[3].Bart[personajecount].pregunta; 
+    document.getElementById('respuesta1').innerHTML = data.preguntas[3].Bart[personajecount].Opciones[0].texto;
+    document.getElementById('respuesta2').innerHTML = data.preguntas[3].Bart[personajecount].Opciones[1].texto;
+    document.getElementById('respuesta3').innerHTML = data.preguntas[3].Bart[personajecount].Opciones[2].texto;
+    document.getElementById('Opcion1').value = data.preguntas[3].Bart[personajecount].Opciones[0].result;
+    document.getElementById('Opcion2').value = data.preguntas[3].Bart[personajecount].Opciones[1].result;
+    document.getElementById('Opcion3').value = data.preguntas[3].Bart[personajecount].Opciones[2].result;
   
     })
     break;
-    default:     
- 
+    default: 
+    
+    
   }
 
     btnM.addEventListener("click", (Cerrar) => {
     modalCentro.classList.remove("active");
       })
+     
  })
 })
 
   const form = document.getElementById('formulario');
-
   form.addEventListener('submit', function(event) {
     event.preventDefault();
    const respuesta = document.querySelector('input[name="respuesta"]:checked').value;
    const puntaje = document.getElementById('puntaje');
+
     if (respuesta === 'true' ) {
      
         score++;
@@ -117,9 +144,9 @@ personajes.forEach((personaje)=>{
         puntaje.innerHTML ="¡Felcitaciones, has acertado! sumas " + score + " punto(s)"; 
         modalCentro.classList.remove("active");
         console.log(score);
-       
+      
 
-        if (score === 15) {
+        if (score === 15 && intentos <= 0 ) {
           if(confirm("¡Felicitaciones has ganado!¿Deseas jugar de nuevo?")){
               score = 0;
               puntaje.style.visibility = "hidden";
@@ -132,18 +159,19 @@ personajes.forEach((personaje)=>{
                           }
              
                             }else{
-                              if(intentos<=0){  
-                                contenido.innerHTML = "¡Has perdido , no tienes más intentos!"; 
+                              if(intentos === 0 ){  
+                                contenido.innerHTML = "¡ GAME OVER, vuelve a a comenzar el juego!";
+                                btns.style.visibility= "hidden";
+                                console.log(intentos);
                                 intentos = 2 ; 
-                                modalCentro.classList.remove("active");
                               }else{
-                              contenido.innerHTML = "¡Sigue intentando, tu puedes!¡Te queda " + (intentos) + " intentos" ; 
-                              intentos--;
+                                btns.style.visibility= "visible";
+                                contenido.innerHTML = "¡ Sigue intentando,te quedan " + (intentos) + " intento(s) !" ; 
+                                intentos--;
                             }}
                             
   
   })
-
           
      
  
